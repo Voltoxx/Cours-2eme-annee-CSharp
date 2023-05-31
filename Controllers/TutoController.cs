b21fef09100c4156d0255b4c166a504d969870e3
Arthur <arthur.philippe21@gmail.com>
2023-05-31 15:02:22 +0200

    Création de CreateOneTuto, UpdateOneTuto et DeleteOneTuto dans TutoController qui permet de créer, modifier et supprimer un tuto précisement

--- Controllers/TutoController.cs
+++ Controllers/TutoController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using CoursEnC.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoursEnC.Controllers
 {
@@ -40,7 +41,43 @@ namespace CoursEnC.Controllers
             return NotFound("Pas la bonne ID bg");
         }
 
-        
-        
+        // POST : Tuto/CreateOneTuto
+        [HttpPost("CreateOneTuto")]
+
+        public ActionResult<Tuto> CreateOneTuto(Tuto tuto)
+        {
+            context.tuto.Add(tuto);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        // PUT : Tuto/UpdateOneTuto
+        [HttpPut("UpdateOneTuto")]
+
+        public ActionResult<Tuto> UpdateOneTuto(Tuto tuto)
+        {
+            context.Entry(tuto).State = EntityState.Modified;
+            context.SaveChanges();
+            return tuto;
+        }
+
+        // DELETE : Tuto/DeleteOneTuto
+        [HttpDelete("DeleteOneTuto/{id}")]
+        public ActionResult<Tuto> DeleteOneTuto(int id)
+        {
+            context.tuto.Remove(context.tuto.Find(id));
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("DeleteOneTuto")]
+        public ActionResult<Tuto> DeleteOneTuto(Tuto tuto)
+        {
+            context.tuto.Remove(tuto);
+            context.SaveChanges();
+            return Ok();
+        }
+
+
     }
 }
